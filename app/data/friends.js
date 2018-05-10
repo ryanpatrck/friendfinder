0458333aa944691947118ecf3530ae79d198e7b1
Ryan <Admin@MacBook-Pro.local>
2018-05-09 20:25:07 -0400

    friend

---
+++ app/data/friends.js
@@ -0,0 +1,158 @@
+var friends = [
+    {
+        name: "Ryan Siedel",
+        photo: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?cs=srgb&dl=adult-autumn-blue-1043474.jpg&fm=jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ] 
+        
+    }, { 
+    name: "Jeff Newman",
+    photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?cs=srgb&dl=black-and-white-fun-happy-91227.jpg&fm=jpg",
+    scores: [
+         "1",
+         "5",
+         "5",
+         "1",
+         "5",
+         "5",
+         "1",
+         "1",
+         "1",
+         "5"
+    ]
+}, { 
+    name: "Larry David",
+    photo: "https://ia.media-imdb.com/images/M/MV5BMTg1Njg4MzQxNV5BMl5BanBnXkFtZTcwNDgzNDUwMw@@._V1_UY317_CR7,0,214,317_AL_.jpg",
+    scores: [
+         "2",
+         "4",
+         "1",
+         "1",
+         "1",
+         "1",
+         "1",
+         "2",
+         "1",
+         "5"
+    ]
+
+},{
+    name: "Natalya Rudakova",
+    photo: "https://thumbs.dreamstime.com/b/natalya-rudakova-world-premiere-transporter-held-planet-hollywood-casino-resort-las-vegas-united-states-54923822.jpg",
+    scores: [
+         "5",
+         "2",
+         "3",
+         "1",
+         "5",
+         "1",
+         "3",
+         "1",
+         "1",
+         "5"
+    ]
+}, {
+    name: "Mark Harmon",
+    photo: "https://www.biography.com/.image/t_share/MTQ1Mjk5MjEyMDQzODg4MDI0/mark-harmon---st-elsewhere.jpg",
+    scores: [
+         "4",
+         "3",
+         "5",
+         "1",
+         "1",
+         "5",
+         "5",
+         "1",
+         "1",
+         "1"
+    ]
+},{
+    name: "Jessica Alba",
+    photo: "https://ia.media-imdb.com/images/M/MV5BODYxNzE4OTk5Nl5BMl5BanBnXkFtZTcwODYyMDYzMw@@._V1_UY317_CR12,0,214,317_AL_.jpg",
+    scores: [
+         "5",
+         "1",
+         "2",
+         "1",
+         "3",
+         "4",
+         "1",
+         "3",
+         "3",
+         "4"
+    ]
+}, {
+    name: "Kris Humphries",
+    photo: "https://www.denverpost.com/wp-content/uploads/2016/05/20120409_024022_krishumphriesfor1.jpg?w=620",
+    scores: [
+         "1",
+         "1",
+         "5",
+         "5",
+         "5",
+         "5",
+         "5",
+         "5",
+         "5",
+         "1"
+    ]
+
+}, {
+    name: "Owen Wilson",
+    photo: "http://www.media3.hw-static.com/media/2017/12/wenn_owenwilson_122917-1800x1200.jpg",
+    scores: [
+         "1",
+         "1",
+         "5",
+         "1",
+         "3",
+         "2",
+         "5",
+         "1",
+         "1",
+         "1"
+    ]
+}, {
+    name: "Chace Crawford",
+    photo: "https://media1.popsugar-assets.com/files/thumbor/9M1vW8wlNXraU7Z12on4x2p1Ets/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2015/10/06/618/n/1922398/e7cb3795_edit_img_image_16360412_1444139103/i/Chace-Crawford-Pictures-October-2015.png",
+    scores: [
+         "5",
+         "4",
+         "4",
+         "2",
+         "4",
+         "4",
+         "3",
+         "2",
+         "3",
+         "3"
+    ]
+}, {
+    name: "Howard Stern",
+    photo: "https://i.kinja-img.com/gawker-media/image/upload/s--Ruxy1Bqv--/c_scale,f_auto,fl_progressive,q_80,w_800/r7i3vwrihbwxqiptkg80.jpg",
+    scores: [
+         "5",
+         "4",
+         "4",
+         "2",
+         "4",
+         "4",
+         "3",
+         "2",
+         "3",
+         "3"
+    ]
+}
+
+
+]
